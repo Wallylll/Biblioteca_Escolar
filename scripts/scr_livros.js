4a647dd8387fb7c12654e59465e164c195b0304b
FwSenai10 <francisco.w.oliveira10@aluno.senai.br>
2025-10-21 08:51:01 -0300

    V1.0

---
+++ scripts/scr_livros.js
@@ -0,0 +1,54 @@
+const livros = [
+  {
+    id: "bk001",
+    nome: "A Casa das Palavras",
+    autor: "Helena Duarte",
+    editora: "Editora Aurora",
+    dataLancamento: "2012-05-10",
+    genero: "Ficção Literária",
+    linguagem: "Português",
+    ilustracao: "https://covers.openlibrary.org/b/id/8225261-L.jpg"
+  },
+  {
+    id: "bk002",
+    nome: "Noites na Biblioteca",
+    autor: "Rafael Moreira",
+    editora: "Grifo Editorial",
+    dataLancamento: "2018-09-21",
+    genero: "Mistério",
+    linguagem: "Português",
+    ilustracao: "https://covers.openlibrary.org/b/id/9872712-L.jpg"
+  },
+  {
+    id: "bk003",
+    nome: "Manual do Jovem Leitor",
+    autor: "Carla Menezes",
+    editora: "Saber & Companhia",
+    dataLancamento: "2020-03-15",
+    genero: "Educação / Didático",
+    linguagem: "Português",
+    ilustracao: "https://covers.openlibrary.org/b/id/11153266-L.jpg"
+  },
+  {
+    id: "bk004",
+    nome: "Histórias da Rua Velha",
+    autor: "João Henrique Alves",
+    editora: "Lumen Press",
+    dataLancamento: "2006-11-02",
+    genero: "Contos",
+    linguagem: "Português",
+    ilustracao: "https://covers.openlibrary.org/b/id/240727-L.jpg"
+  },
+  {
+    id: "bk005",
+    nome: "Atlas das Palavras Perdidas",
+    autor: "Marina Freitas",
+    editora: "Ponto & Vírgula",
+    dataLancamento: "2023-01-30",
+    genero: "Fantasia",
+    linguagem: "Português",
+    ilustracao: "https://covers.openlibrary.org/b/id/12610214-L.jpg"
+  }
+];
+
+export default livros;
